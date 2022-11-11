d854a5d4f144e361394e41eddcaf65d7ce24a998
Mohsen Rzna <ash.wxrz@hotmail.com>
2022-11-12 00:28:56 +0100

    Refactoring for the ext function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,12 +6,13 @@
 fun main() {
     val input1 = "2x^3 + 4x^2 + 9x"
     println(derivePoly(input1))
+    println(input1.derivative())
 }
 
 fun derivePoly(inputString: String): String {
     with(inputString) {
-
         val input = split(" + ")
+
         val firstOutput = input
             .map { it.split("x").first() } // ["2", "4", "9"]
 
@@ -25,22 +26,22 @@ fun derivePoly(inputString: String): String {
     }
 }
 
-
 /**
  * Extension function to derive a polynomial
  * @return the derivative of the polynomial
  */
 
-/*
-fun String.derivePoly(): String {
-    val myItem = split(" + ")
+fun String.derivative(): String {
+    val input = split(" + ")
 
-    val firstItem = myItem[0].first().digitToInt() * 3
-    val firstItemLast = myItem[0].last().digitToInt() - 1
+    val firstOutput = input
+        .map { it.split("x").first() } // ["2", "4", "9"]
 
-    val secondItem = myItem[1].split("^").first().find { it == '4' }?.digitToInt()?.times(2)
-    val thirdItem = myItem[2].first().digitToInt()
+    val firstItemLast = input[0].last() - 1 // 2
+    val firstItem = firstOutput[0].toInt() * 3 // 6
+
+    val secondItem = firstOutput[1].toInt() * 2 // 8
+    val thirdItem = firstOutput[2].toInt() // 9
 
     return "${firstItem}x^${firstItemLast} + ${secondItem}x + $thirdItem"
 }
-*/
